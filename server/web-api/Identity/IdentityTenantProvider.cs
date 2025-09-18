6d378f82039dfe8f1ca284f8395840695f2c8a23
AlexAraldi <alexanderaraldi@gmail.com>
2025-09-18 03:14:00 -0300

    feat: Adicionado configurações do Identity

---
+++ server/web-api/Identity/IdentityTenantProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Claims;
+using eAgenda.Core.Dominio.ModuloAutenticacao;
+
+namespace Gestao_de_Estacionamentos.WebApi.Identity;
+
+public class IdentityTenantProvider(IHttpContextAccessor contextAccessor) : ITenantProvider
+{
+    public Guid? UsuarioId
+    {
+        get
+        {
+            var claimId = contextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claimId is null)
+                return null;
+
+            return Guid.Parse(claimId.Value);
+        }
+    }
+
+    public bool IsInRole(string role)
+    {
+        return contextAccessor.HttpContext?.User?.IsInRole(role) ?? false;
+    }
+}
