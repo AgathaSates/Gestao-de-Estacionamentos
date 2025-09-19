2c4e6153d72bb591be1493d49556e6be625dfad5
AlexAraldi <alexanderaraldi@gmail.com>
2025-09-19 17:16:39 -0300

    feat: Adicionado camada da API [Autentcação]

---
+++ server/web-api/Models/ModuloAutenticacao/RegistrarUsuarioRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Gestao_de_Estacionamentos.WebApi.Models.ModuloAutenticacao;
+
+public record RegistrarUsuarioRequest(
+    string NomeCompleto,
+    string Email,
+    string Senha,
+    string ConfirmarSenha
+);
