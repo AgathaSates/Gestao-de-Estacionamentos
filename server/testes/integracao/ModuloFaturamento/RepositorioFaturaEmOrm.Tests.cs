ec6c4ee3210f5e01853965f2d038b25ee9ef7b38
AlexAraldi <alexanderaraldi@gmail.com>
2025-09-22 01:06:09 -0300

    feat: Adicionado Tests Unitário e Integração[Faturamento]

---
+++ server/testes/integracao/ModuloFaturamento/RepositorioFaturaEmOrm.Tests.cs
@@ -0,0 +1,101 @@
+﻿using Gestao_de_Estacionamentos.Core.Dominio.ModuloFaturamento;
+using Gestao_de_Estacionamentos.Core.Dominio.ModuloRecepcao.EntidadeVeiculo;
+using Gestao_de_Estacionamentos.Core.Dominio.ModuloRecepcao;
+using Gestao_de_Estacionamentos.Testes.Integracao.Compartilhado;
+
+namespace Gestao_de_Estacionamentos.Testes.Integracao.ModuloFaturamento
+{
+
+    [TestClass]
+    [TestCategory("Testes de Integração de Faturamento")]
+    public class RepositorioFaturamentoEmOrm : TestFixture
+    {
+
+        [TestMethod]
+        public void Deve_Calcular_Valor_Fatura_Corretamente()
+        {
+            // Arrange
+            int numeroDiarias = 5;
+            decimal valorDiaria = 20000; // Valor em centavos (R$ 200,00)
+            decimal valorEsperado = (numeroDiarias * valorDiaria) / 100; // Convertendo para reais
+
+            // Act
+            decimal valorCalculado = _repositorioFaturamento!.CalcularValorFatura(numeroDiarias, valorDiaria);
+
+            // Assert
+            Assert.AreEqual(valorEsperado, valorCalculado);
+        }
+
+        [TestMethod]
+        public async Task Deve_Selecionar_Faturas_Por_Periodo_Corretamente()
+        {
+            // Arrange
+            var dataInicio = DateTime.UtcNow;
+            var dataFim = dataInicio.AddDays(2);
+
+            Veiculo veiculo = new Veiculo("TEST567", "modelo", "cor", "vidro quebrado");
+            Veiculo veiculo2 = new Veiculo("TEST000", "modelo2", "cor2", "vidro quebrado2");
+            CheckIn checkIn = new CheckIn(veiculo, "11122233345", "Juninho");
+            CheckIn checkIn2 = new CheckIn(veiculo2, "11122233344", "Maria");
+
+            await _repositorioRecepcao!.CadastrarAsync(checkIn);
+            await _repositorioRecepcao!.CadastrarAsync(checkIn2);
+            await _dbContext!.SaveChangesAsync();
+
+            Fatura fatura1 = new Fatura(checkIn.Ticket.Id, checkIn.Veiculo.Placa, dataInicio, dataFim);
+            Fatura fatura2 = new Fatura(checkIn2.Ticket.Id, checkIn2.Veiculo.Placa, dataInicio.AddDays(1), dataFim.AddDays(1));
+
+            var faturasCadastradas = new List<Fatura> { fatura1, fatura2 };
+            var faturasEsperadasOrdenadas = new List<Fatura> { fatura1 }.OrderBy(f => f.DataEntrada);
+
+            await _repositorioFatura!.CadastrarEntidadesAsync(faturasCadastradas);
+            await _dbContext.SaveChangesAsync();
+
+            // Act
+            List<Fatura> faturasEncontradas = await _repositorioFatura!
+                .SelecionarFaturasPorPeriodoAsync(dataInicio, dataFim);
+
+            // Assert
+            CollectionAssert.AreEqual(faturasEsperadasOrdenadas.ToList(), faturasEncontradas);
+        }
+
+        [TestMethod]
+        public async Task Deve_Selecionar_Fatura_Por_Id_Corretamente()
+        {
+            // Arrange
+            var dataInicio = DateTime.UtcNow;
+            var dataFim = dataInicio.AddDays(2);
+
+            Veiculo veiculo = new Veiculo("TEST567", "modelo", "cor", "vidro quebrado");
+            CheckIn checkIn = new CheckIn(veiculo, "11122233345", "Juninho");
+
+            await _repositorioRecepcao!.CadastrarAsync(checkIn);
+            await _dbContext!.SaveChangesAsync();
+
+            Fatura fatura1 = new Fatura(checkIn.Ticket.Id, checkIn.Veiculo.Placa, dataInicio, dataFim);
+
+            await _repositorioFatura!.CadastrarAsync(fatura1);
+            await _dbContext.SaveChangesAsync();
+
+            // Act
+            Fatura? faturaEncontrada = await _repositorioFatura!
+                .SelecionarRegistroPorIdAsync(fatura1.Id);
+
+            // Assert
+            Assert.AreEqual(fatura1, faturaEncontrada);
+        }
+
+        [TestMethod]
+        public async Task Deve_Selecionar_Por_Id_Retornar_Nulo_Quando_Nao_Encontrar()
+        {
+            // Arrange
+            var idInexistente = Guid.NewGuid();
+            // Act
+            Fatura? faturaEncontrada = await _repositorioFatura!
+                .SelecionarRegistroPorIdAsync(idInexistente);
+            // Assert
+            Assert.IsNull(faturaEncontrada);
+        }
+
+    }
+}
---
+++ server/testes/unidade/ModuloFaturamento/FaturaTests.cs
@@ -0,0 +1,60 @@
+﻿using Gestao_de_Estacionamentos.Core.Dominio.ModuloFaturamento;
+
+namespace Gestao_de_Estacionamentos.Testes.Unidade.ModuloFaturamento
+{
+    [TestClass]
+    [TestCategory("Teste de Unidade da Entidade: Fatura")]
+    public class FaturaTests
+    {
+        [TestMethod]
+        public void Deve_Criar_Fatura_Corretamente()
+        {
+            // Arrange
+            var ticketId = Guid.NewGuid();
+            var placaVeiculo = "ABC1234";
+            var dataEntrada = new DateTime(2023, 10, 1);
+            var dataSaida = new DateTime(2023, 10, 5);
+
+            // Act
+            var fatura = new Fatura(ticketId, placaVeiculo, dataEntrada, dataSaida);
+            // Assert
+
+            Assert.IsNotNull(fatura.Id);
+            Assert.AreEqual(ticketId, fatura.TicketId);
+            Assert.AreEqual(placaVeiculo, fatura.PlacaVeiculo);
+            Assert.AreEqual(dataEntrada, fatura.DataEntrada);
+            Assert.AreEqual(dataSaida, fatura.DataSaida);
+            Assert.AreEqual(0, fatura.Valortotal);
+        }
+
+        [TestMethod]
+        public void Deve_Calcular_Valor_Total_Corretamente()
+        {
+            // Arrange
+            var fatura = new Fatura(Guid.NewGuid(), "ABC1234", DateTime.Now, DateTime.Now.AddDays(3));
+            int numeroDiarias = 4;
+            decimal valorDiaria = 2000;
+
+            // Act
+            fatura.CalcularValorTotal(numeroDiarias, valorDiaria);
+
+            // Assert
+            Assert.AreEqual(80, fatura.Valortotal);
+        }
+
+        [TestMethod]
+        public void Deve_Calcular_Numero_Diarias_Corretamente()
+        {
+            // Arrange
+            var fatura = new Fatura(Guid.NewGuid(), "ABC1234", DateTime.Now, DateTime.Now.AddDays(3));
+            var dataEntrada = new DateTime(2023, 10, 1);
+            var dataSaida = new DateTime(2023, 10, 5);
+
+            // Act
+            int numeroDiarias = fatura.CalcularNumeroDiarias(dataEntrada, dataSaida);
+
+            // Assert
+            Assert.AreEqual(5, numeroDiarias);
+        }
+    }
+}
