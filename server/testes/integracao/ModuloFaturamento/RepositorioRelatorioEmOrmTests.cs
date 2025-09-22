1f070f66a07631a1dae027a769631400f4d07ce1
AlexAraldi <alexanderaraldi@gmail.com>
2025-09-22 01:08:13 -0300

    feat: Adicionado Tests Unitário e Integração Entidade Relatório[Faturamento]

---
+++ server/testes/integracao/ModuloFaturamento/RepositorioRelatorioEmOrmTests.cs
@@ -0,0 +1,75 @@
+﻿using Gestao_de_Estacionamentos.Core.Dominio.ModuloFatura;
+using Gestao_de_Estacionamentos.Core.Dominio.ModuloFaturamento;
+using Gestao_de_Estacionamentos.Core.Dominio.ModuloRecepcao.EntidadeVeiculo;
+using Gestao_de_Estacionamentos.Core.Dominio.ModuloRecepcao;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestao_de_Estacionamentos.Testes.Integracao.ModuloFaturamento
+{
+    public class RepositorioRelatorioEmOrmTests
+    {
+        [TestMethod]
+        public async Task Deve_Cadastrar_Relatorio_Corretamente()
+        {
+            // Arrange
+            var dataInicio = DateTime.UtcNow;
+            var dataFim = dataInicio.AddDays(2);
+
+            Veiculo veiculo = new Veiculo("TEST567", "modelo", "cor", "vidro quebrado");
+            CheckIn checkIn = new CheckIn(veiculo, "11122233345", "Juninho");
+
+            await _repositorioRecepcao!.CadastrarAsync(checkIn);
+            await _dbContext!.SaveChangesAsync();
+
+            Fatura fatura1 = new Fatura(checkIn.Ticket.Id, checkIn.Veiculo.Placa, dataInicio, dataFim);
+            Fatura fatura2 = new Fatura(checkIn.Ticket.Id, checkIn.Veiculo.Placa, dataInicio, dataFim);
+            var faturasCadastradas = new List<Fatura> { fatura1, fatura2 };
+
+            await _repositorioFatura!.CadastrarEntidadesAsync(faturasCadastradas);
+            await _dbContext.SaveChangesAsync();
+
+            Relatorio relatorio = new Relatorio(dataInicio, dataFim, faturasCadastradas);
+            // Act
+            await _repositorioRelatorio!.CadastrarAsync(relatorio);
+            await _dbContext.SaveChangesAsync();
+
+            Relatorio? relatorioEncontrado = await _repositorioRelatorio!
+                .SelecionarRegistroPorIdAsync(relatorio.Id);
+
+            // Assert
+            Assert.AreEqual(relatorio, relatorioEncontrado);
+        }
+
+        [TestMethod]
+        public async Task Deve_Gerar_Relatorio_Corretamente()
+        {
+            // Arrange
+            var dataInicio = DateTime.UtcNow;
+            var dataFim = dataInicio.AddDays(2);
+
+            Veiculo veiculo = new Veiculo("TEST567", "modelo", "cor", "vidro quebrado");
+            CheckIn checkIn = new CheckIn(veiculo, "11122233345", "Juninho");
+
+            await _repositorioRecepcao!.CadastrarAsync(checkIn);
+            await _dbContext!.SaveChangesAsync();
+
+            Fatura fatura1 = new Fatura(checkIn.Ticket.Id, checkIn.Veiculo.Placa, dataInicio, dataFim);
+            Fatura fatura2 = new Fatura(checkIn.Ticket.Id, checkIn.Veiculo.Placa, dataInicio, dataFim);
+
+            var faturasCadastradas = new List<Fatura> { fatura1, fatura2 };
+
+            await _repositorioFatura!.CadastrarEntidadesAsync(faturasCadastradas);
+            await _dbContext.SaveChangesAsync();
+
+            Relatorio relatorioEsperado = new Relatorio(dataInicio, dataFim, faturasCadastradas);
+
+            // Act
+            Relatorio relatorioGerado = await _repositorioRelatorio!
+                .GerarRelatorioFinanceiroAsync(dataInicio, dataFim, faturasCadastradas);
+
+            // Assert
+            Assert.AreEqual(relatorioEsperado.ValorTotal, relatorioGerado.ValorTotal);
+        }
+
+    }
+}
---
+++ server/testes/unidade/ModuloFaturamento/RelatorioTests.cs
@@ -0,0 +1,54 @@
+﻿using Gestao_de_Estacionamentos.Core.Dominio.ModuloFaturamento;
+
+namespace Gestao_de_Estacionamentos.Testes.Unidade.ModuloFaturamento
+{
+    [TestClass]
+    [TestCategory("Teste de Unidade da Entidade: Relatório")]
+    public class RelatorioTests
+    {
+        [TestMethod]
+        public void Deve_Criar_Relatorio_Corretamente()
+        {
+            //Arrange
+            var faturas = new List<Fatura>
+    {
+        new Fatura(Guid.NewGuid(), "ABC1234", DateTime.Now.AddDays(-5), DateTime.Now.AddDays(-2)),
+        new Fatura(Guid.NewGuid(), "XYZ5678", DateTime.Now.AddDays(-4), DateTime.Now.AddDays(-1))
+    };
+
+            var dataInicial = DateTime.Now.AddDays(-5);
+            var dataFinal = DateTime.Now;
+
+            //Act
+            var relatorio = new Relatorio(dataInicial, dataFinal, faturas);
+
+            //Assert
+            Assert.IsNotNull(relatorio.Id);
+            Assert.AreEqual(dataInicial, relatorio.DataInicial);
+            Assert.AreEqual(dataFinal, relatorio.DataFinal);
+            Assert.AreEqual(faturas, relatorio.Faturas);
+            Assert.AreEqual(0, relatorio.ValorTotal);
+        }
+
+        [TestMethod]
+        public void Deve_Gerar_Valor_Total_Corretamente()
+        {
+            //Arrange
+            var faturas = new List<Fatura>
+    {
+        new Fatura(Guid.NewGuid(), "ABC1234", DateTime.Now.AddDays(-5), DateTime.Now.AddDays(-2)),
+        new Fatura(Guid.NewGuid(), "XYZ5678", DateTime.Now.AddDays(-4), DateTime.Now.AddDays(-1))
+    };
+
+            faturas[0].CalcularValorTotal(4, 2000);
+            faturas[1].CalcularValorTotal(4, 1500);
+            var relatorio = new Relatorio(DateTime.Now.AddDays(-5), DateTime.Now, faturas);
+
+            //Act
+            relatorio.GerarValorTotal();
+
+            //Assert
+            Assert.AreEqual(140, relatorio.ValorTotal);
+        }
+    }
+}
